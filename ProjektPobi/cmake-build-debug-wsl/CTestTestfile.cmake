0825a6281d222c339e24cb059c5378babbd1f47b
Abidingbrush787 <161648027+Abidingbrush787@users.noreply.github.com>
2024-06-19 22:29:59 +0200

    Delete build

--- ProjektPobi/cmake-build-debug-wsl/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aleks/POBI/ProjektPobi
-# Build directory: /home/aleks/POBI/ProjektPobi/cmake-build-debug-wsl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test "/home/aleks/POBI/ProjektPobi/cmake-build-debug-wsl/TestProject" "--report_level=detailed" "--log_level=all" "--color_output=yes")
-set_tests_properties(Test PROPERTIES  _BACKTRACE_TRIPLES "/home/aleks/POBI/ProjektPobi/CMakeLists.txt;83;add_test;/home/aleks/POBI/ProjektPobi/CMakeLists.txt;0;")
